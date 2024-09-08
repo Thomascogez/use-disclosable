3cf84c612c193ed3e83456fda5cc16e90475e22f
Thomascogez <thomas.cogez@gmail.com>
2024-09-08 14:21:46 +0200

    fix: provide default getServerSnapshot function in order to make next.js build works

--- packages/use-disclosable/lib/disclosable-root.tsx
+++ packages/use-disclosable/lib/disclosable-root.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { useDisclosable } from "./use-disclosable";
 
 export const DisclosableRoot: React.FC<unknown> = () => {
--- packages/use-disclosable/lib/use-disclosable.ts
+++ packages/use-disclosable/lib/use-disclosable.ts
@@ -1,8 +1,13 @@
 import { useSyncExternalStore } from "react";
 
 import { disclosableStore } from "./disclosable-store";
+import type { DisclosableStore } from "./types";
+
+// This allow to make next.js build work
+const stubServerSnapshot = (): DisclosableStore["disclosables"] => ({});
+
 export const useDisclosable = () => {
-    const disclosables = useSyncExternalStore(disclosableStore.subscribe, disclosableStore.getDisclosables);
+    const disclosables = useSyncExternalStore(disclosableStore.subscribe, disclosableStore.getDisclosables, stubServerSnapshot);
 
     return {
         disclosables,
